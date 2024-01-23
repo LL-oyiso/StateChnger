7f24c84cbabbed188f39261a97d3791d0cf6146c
LL-oyiso <theo.tose.tt@gmail.com>
2024-01-23 09:38:56 +0200

    New Pink button added

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,10 @@ function App() {
       <div className ="w-full h-screen" style ={{backgroundColor : color}}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 '>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl'>
+            {/* The purpose of a callback function is to change a piece of state that part of a parent componet */}
            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black' onClick ={() => setColor('blue')}>Blue</button>
            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black' onClick={() => setColor('grey')}>Grey</button>
+           <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black' onClick ={() => setColor('pink')}>Pink</button>
           </div>
         </div>
       
